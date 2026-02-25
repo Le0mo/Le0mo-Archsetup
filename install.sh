a592c8bf67c586c6193823f2be474ccbd12c911b
Le0mo <inkcuspluma@foxmail.com>
2026-02-26 05:39:50 +0800

    Update configurations

--- install.sh
+++ install.sh
@@ -12,20 +12,15 @@ done
 
 rm -rf "$HOME/.local/share/shorin-niri/dotfiles/.config/waybar-niri-Win11Like"
 
-for item in "$HOME/.Github/Le0mo-Archsetup/dotfiles/.local/bin/"*; do
-    name=$(basename "$item")
-    rm -rf "$HOME/.local/share/shorin-niri/dotfiles/.local/bin/$name"
-    ln -s "$HOME/.Github/Le0mo-Archsetup/dotfiles/.local/bin/$name" "$HOME/.local/share/shorin-niri/dotfiles/.local/bin/$name"
-done
+ln -sf "$HOME/.Github/Le0mo-Archsetup/dotfiles/.local/bin/"* "$HOME/.local/share/shorin-niri/dotfiles/.local/bin/"
 
 rm -rf "$HOME/.local/share/shorin-niri/wallpapers"
 ln -s "$HOME/.Github/Le0mo-Archsetup/wallpapers" "$HOME/.local/share/shorin-niri/wallpapers"
 
 chmod +x "$HOME/.config/waybar/scripts/island/"*
 
-cd $HOME/.Github/Le0mo-Archsetup/
-cp Le0mo-update ~/.local/bin/
-chmod +x ~/.local/bin/Le0mo-update
+cd $HOME/.local/bin/
+chmod +x $HOME/.local/bin/Le0mo-update
 
 mv "$HOME/.Github/Le0mo-Archsetup/冬眠-司南.flac" "$HOME/Music/"
 mkdir -p "$HOME/.lyrics"
