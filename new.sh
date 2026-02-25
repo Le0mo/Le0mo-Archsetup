c3efb1c3c92cf136463f86b46f9941aa2ccb9453
Le0mo <inkcuspluma@foxmail.com>
2026-02-26 05:25:23 +0800

    Update configurations

--- new.sh
+++ new.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 for item in "$HOME/.Github/Le0mo-Archsetup/dotfiles/.config/"*; do
     name=$(basename "$item")
-    rm -rf "$HOME/.local/share/shorin-niri/dotfiles/.config/$name"
-    ln -s "$HOME/.Github/Le0mo-Archsetup/dotfiles/.config/$name" "$HOME/.local/share/shorin-niri/dotfiles/.config/$name"
+    if [ -e "$HOME/.local/share/shorin-niri/dotfiles/.config/$name" ] || [ -L "$HOME/.local/share/shorin-niri/dotfiles/.config/$name" ]; then
+        rm -rf "$HOME/.local/share/shorin-niri/dotfiles/.config/$name"
+        ln -s "$HOME/.Github/Le0mo-Archsetup/dotfiles/.config/$name" "$HOME/.local/share/shorin-niri/dotfiles/.config/$name"
+    else
+        rm -rf "$HOME/.config/$name"
+        ln -s "$HOME/.Github/Le0mo-Archsetup/dotfiles/.config/$name" "$HOME/.config/$name"
+    fi
 done
 
 rm -rf "$HOME/.local/share/shorin-niri/dotfiles/.config/waybar-niri-Win11Like"
